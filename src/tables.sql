042fa87acc99db54b0974fc078ef2431a81dd41b
Darrenfisherlol <darren.nov@gmail.com>
2024-09-26 13:30:38 -0500

    drafting up the basics of a coffee show and the data it will generate

---
+++ src/tables.sql
@@ -0,0 +1,148 @@
+/*
+--
+-- Connectin to GCP
+--
+--
+*/
+
+
+
+
+CREATE TABLE Customer(
+    CustomerID INT NOT NULL AUTO_INCREMENT,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    PRIMARY KEY (CustomerID)
+)
+
+CREATE TABLE Store(
+    StoreID INT NOT NULL AUTO_INCREMENT,
+    StoreName VARCHAR(100) NOT NULL,
+    [Address] VARCHAR(100) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    [State] VARCHAR(50) NOT NULL,
+    ZipCode VARCHAR(5) NOT NULL
+    PRIMARY KEY (StoreID)
+)
+
+CREATE TABLE Employee(
+    EmployeeID INT NOT NULL,
+    ManagerID INT,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    StoreID VARCHAR(100) NOT NULL,
+    PRIMARY KEY (EmployeeID),
+    FOREIGN KEY (StoreID) REFERENCES Store(StoreID)
+)
+
+CREATE TABLE Order(
+    OrderID INT NOT NULL AUTO_INCREMENT,
+    EmployeeID INT NOT NULL,
+    CustomerID INT NOT NULL,
+    StoreID INT NOT NULL,
+    OrderDate DATE,
+    TotalAmount float,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+    FOREIGN KEY (StoreID) REFERENCES Store(StoreID)
+)
+
+CREATE TABLE Product(
+    ProductID INT NOT NULL AUTO_INCREMENT,
+    [Name] VARCHAR(100) NOT NULL,
+    Cost VARCHAR(100) NOT NULL,
+    Quantity float,
+    PRIMARY KEY (ProductID)
+)
+
+
+CREATE TABLE SaleProduct(
+    SaleProductID INT NOT NULL AUTO_INCREMENT,
+    OrderID INT NOT NULL,
+    ProductID INT NOT NULL,
+    PaymentID INT NOT NULL,
+    Quantity float,
+    Price float,
+    PRIMARY KEY (SaleProductID),
+    FOREIGN KEY (OrderID) REFERENCES Order(OrderID)
+    FOREIGN KEY (PaymentID) REFERENCES Payment(PaymentID)
+    FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+)
+
+
+/*
+--
+--
+--
+--
+*/
+
+
+
+
+/*
+--
+--
+--
+--
+*/
+
+
+CREATE TABLE Payment(
+    PaymentID INT NOT NULL AUTO_INCREMENT,
+    PaymentName VARCHAR(100) NOT NULL,
+    Fee FLOAT NOT NULL,
+    PRIMARY KEY (PaymentID)
+)
+
+
+
+/*
+--
+--
+--
+--
+*/
+
+
+CREATE TABLE Vendor(
+    VendorID INT NOT NULL AUTO_INCREMENT,
+    [Name] VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(10) NOT NULL,
+    [Address] VARCHAR(100) NOT NULL,
+    [State] VARCHAR(100) NOT NULL,
+    [City] VARCHAR(100) NOT NULL,
+    ZipCode VARCHAR(5) NOT NULL,
+    PaymentMethod VARCHAR(100) NOT NULL,
+    PRIMARY KEY (VendorID)
+)
+
+CREATE TABLE RawInventory(
+    RawInventoryID INT NOT NULL AUTO_INCREMENT,
+    Material VARCHAR(100) NOT NULL,
+    Quantity INT NOT NULL,
+    PRIMARY KEY (RawInventoryID)
+)
+
+CREATE TABLE InventoryOrders(
+    InventoryOrderId INT NOT NULL AUTO_INCREMENT,
+    VendorID INT NOT NULL,
+    EmployeeID INT NOT NULL,
+    RawInventoryID INT NOT NULL,
+    PRIMARY KEY (InventoryOrderId),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID),
+    FOREIGN KEY (RawInventoryID) REFERENCES RawInventory(RawInventoryID)
+)
+
+CREATE TABLE ProductInput(
+    ProductInputId INT NOT NULL AUTO_INCREMENT,
+    ProductID INT NOT NULL,
+    RawInventoryID INT NOT NULL,
+    Quantity INT NOT NULL,
+    PRIMARY KEY (InventoryOrderId),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID),
+    FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
+    FOREIGN KEY (RawInventoryID) REFERENCES RawInventory(RawInventoryID)
+)
